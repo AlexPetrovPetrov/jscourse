aa7e7040472d19c400f2609cb923499a7ff2c90c
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2023-12-05 19:43:16 +0200

    making all seven primitive types of vars

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,6 +1,21 @@
-let country = "Bulgaria"
-let continent = "Europe"
-let population = 7
-console.log(country)
-console.log(continent)
+let person = 'large'
+let population = 100000
+let gnome = true
+let kfc
+let box = null
+console.log(typeof person)
+console.log(typeof population)
+console.log(typeof gnome)
+console.log(typeof kfc)
+console.log(typeof box)
 console.log(population)
+let bigNumber = 13525613180987654321242443465768696070n
+console.log(typeof bigNumber)
+console.log(bigNumber * bigNumber)
+let number = 13525613180987654321242443465768696070
+console.log(number * number)
+population = false
+console.log(typeof population)
+let numberA = 5
+let numberB = 15
+console.log(numberA + numberB)
