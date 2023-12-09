abfefc08ee0f599bbbf074733000a6614bf9a262
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2023-12-09 18:36:14 +0200

    using math operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,32 +1,15 @@
-let person = 'large'
-let population = 100000
-let gnome = true
-let kfc
-let box = null
-console.log(typeof person)
-console.log(typeof population)
-console.log(typeof gnome)
-console.log(typeof kfc)
-console.log(typeof box)
-console.log(population)
-let bigNumber = 13525613180987654321242443465768696070n
-console.log(typeof bigNumber)
-console.log(bigNumber * bigNumber)
-let number = 13525613180987654321242443465768696070
-console.log(number * number)
-population = false
-console.log(typeof population)
-let numberA = 5
-let numberB = 15
-console.log(numberA + numberB)
-let pumkin = 'seeds'
-pumkin = 'seasoning'
-const birthYear = 1996
-// birthYear = 2011
-var eggplant = 'slices'
-eggplant = 'organicOil'
-let age = 13
-const name = 'Alex'
-age = name
-console.log(typeof age)
-
+//Math operators: + - * / ** ^
+const petarAge = 20
+const alexAge = 12
+console.log(alexAge + petarAge)
+const ageDifernce = petarAge - alexAge
+console.log(ageDifernce)
+const multiplyAges = alexAge * petarAge
+console.log(multiplyAges)
+const divadeAges = alexAge / petarAge
+console.log(divadeAges)
+const expression = 12 + (3 * 4) - 1
+console.log(expression)
+const bit = 2 ** 7
+console.log(bit)
+console.log(expression + bit)
