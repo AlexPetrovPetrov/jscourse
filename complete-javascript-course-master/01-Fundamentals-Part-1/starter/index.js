a8134acc9895f539044e40ff62f3a9250b680b9e
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2023-12-11 20:26:07 +0200

    using if/else to make dessicions

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,11 +1,22 @@
-let age = 12
-let alexInfo = `i'm ${age} years old .`
-console.log(alexInfo)
-let description = `My name
-is
-Alex`
-console.log(description)
-description = 'My name \n\
-is \n\
-Alex'
-console.log(description)
+let alexGradeInMath = 3
+if (alexGradeInMath >= 4.50) {
+    console.log('Great job Alex')
+} else {
+    console.log('You really need to practise!!!!!!!!!')
+}
+let currentSpeed = 61
+const speedLimit = 60
+if (currentSpeed <= speedLimit) {
+    console.log('Have nice ride.')
+} else {
+    console.log('Please slow down.')
+}
+if (currentSpeed < speedLimit) {
+    console.log('Have a nice ride.')
+}
+const name = 'Alex'
+{
+    console.log(name)
+    const age = 42
+}
+console.log(age)
