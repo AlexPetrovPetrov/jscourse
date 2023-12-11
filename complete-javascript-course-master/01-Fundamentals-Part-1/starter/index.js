e12adeb6370ef53e048f6a05ad5adc1e70d11259
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2023-12-11 19:42:46 +0200

    using template literals

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,11 +1,11 @@
-// comparison operators: > < >= <=  === !==
-const a = 3
-const b = 5
-const c = 3
-console.log(a > b)
-console.log(a < b)
-console.log(a >= b)
-console.log(a >= c)
-console.log(a !== c)
-console.log(a === c)
-console.log(b !== c)
+let age = 12
+let alexInfo = `i'm ${age} years old .`
+console.log(alexInfo)
+let description = `My name
+is
+Alex`
+console.log(description)
+description = 'My name \n\
+is \n\
+Alex'
+console.log(description)
