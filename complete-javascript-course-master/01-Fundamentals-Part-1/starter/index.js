96175f1138153c1bf004acecd6eeb65fdc045d0f
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2023-12-13 18:08:36 +0200

    using falsy values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,11 +1,7 @@
-//Type conversion 
-const inputYear = '1994'
-console.log(Number(inputYear) + 18)
-const name = 345
-console.log(String(name) + 'old')
-
-//TYPE coercion
-const snowman = '90'
-console.log(snowman - 50)
-const tree = 87
-console.log(tree + '34')
+// Falsy values: undefined,'',0,NaN
+let age = 0
+if (age < 18) {
+    console.log(`You are just a kid`)
+} else {
+    console.log(`You are an adult`)
+}
