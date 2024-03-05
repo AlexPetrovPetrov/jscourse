92a884918c169704e7a4a2faf6cf0ed7d2942e67
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-03-05 09:54:17 +0200

    doing an exercise

---
+++ school.py
@@ -0,0 +1,7 @@
+number_apples_in_left_hand = 10
+number_apples_in_vase = 20
+print(number_apples_in_left_hand + number_apples_in_vase)
+
+numberCandy = "ten"
+typeCandy = "chocolate candy"
+print(numberCandy + typeCandy)
