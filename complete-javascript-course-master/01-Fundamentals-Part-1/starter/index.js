641b4224a9166c432080535354e17f0314a083da
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-04-01 20:08:53 +0300

    using logical operarors

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,3 +1,11 @@
-const user = 'girl'
-const color = (user === 'girl') ? 'red' : 'blue'
-console.log(color)
+// logical operators && || !
+const hasDriversLicense = true
+const hasGoodVision = true
+console.log(hasDriversLicense && hasGoodVision)
+console.log(!hasGoodVision && hasDriversLicense)
+const shouldDrive = false
+if (hasDriversLicense && hasGoodVision && shouldDrive) {
+    console.log("Sarah should drive")
+} else {
+    console.log("Someone else should driver...")
+}
