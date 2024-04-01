4d4501a8df080649b5fb11ff4ddb930839ad5667
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-04-01 20:57:17 +0300

    using switch statement

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/index.js
@@ -1,11 +1,25 @@
-// logical operators && || !
-const hasDriversLicense = true
-const hasGoodVision = true
-console.log(hasDriversLicense && hasGoodVision)
-console.log(!hasGoodVision && hasDriversLicense)
-const shouldDrive = false
-if (hasDriversLicense && hasGoodVision && shouldDrive) {
-    console.log("Sarah should drive")
-} else {
-    console.log("Someone else should driver...")
+// switch statement
+const day = 'Sunday'
+switch (day) {
+    case 'Monday':
+        console.log("Train in the morning.")
+        console.log("Solve mathematical problems.")
+        console.log("Learn a bit of javascript.")
+        break;
+    case 'Tuesday':
+        console.log("Train in the morning.")
+        console.log("Read some book.")
+        break;
+    case 'Wednesday':
+    case 'Thursday':
+        console.log("Solve mathematical problems.")
+        console.log("`Go out with my friends and have fun.")
+        break;
+    case 'Friday':
+        console.log("Be lazy all day")
+        break;
+    case 'Saturday':
+    case 'Sunday':
+        console.log("Learns some javascript")
+        console.log("Learn new formulas about the 3by3 and 4by4 rubicks cubes.")
 }
