237eb98c1b47206d3e8741441cb8e99558d78895
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-04-05 17:22:25 +0300

    using arrow functions and making exersices

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,20 @@
-// using arrow function
-// const calcAge3 = birthyear => 2037 - birthyear;
-// const age3 = calcAge3(2011);
-// console.log(age3)
+//using arrow function
+const calcAge3 = birthyear => 2037 - birthyear;
+const age3 = calcAge3(2011);
+console.log(age3)
 const yearstillRetirement = (birthYeah, firstName) => {
     const age = 2037 - birthYeah;
     const retirement = 65 - age;
     return `${firstName} retires in ${retirement}
     years`;
 }
+
 console.log(yearstillRetirement(2011, 'Alex'));
+const yearsUntillDriversLicense = (birthYear, firstName) => {
+    const age = 2024 - birthYear;
+    const canBuyCar = 18 - age;
+    return `${firstName}can buy a car in ${canBuyCar} years`;
+}
+console.log(yearsUntillDriversLicense(2011, 'Alex'))
+
 
