03d0b430b3c1648c6e9b32034d78fddbb7da7fb8
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-04-21 17:07:03 +0300

    using arrow functions

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,25 +1,15 @@
-// Basic array operation 
-const friends = ['Yoan', 'Marti', 'Velizar'];
-const newLenght = friends.push('Stefan');
-console.log(friends);
-console.log(newLenght)
-friends.unshift('Dinev');
-console.log(friends);
 
-friends.pop()
-const popped = friends.pop();
-console.log(popped)
-console.log(friends)
-friends.shift()
-console.log(friends)
-
-console.log(friends.indexOf('Stefan'));
-console.log(friends.indexOf('Bill'));
-friends.push(23);
-console.log(friends.includes('Stefan'));
-console.log(friends.includes('Bill'));
-console.log(friends.includes(23));
-
-if (friends.includes('Peter')) {
-    console.log('You have a friend called Peter')
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2024 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years.`;
 }
+console.log(yearsUntilRetirement(1971, 'Petar'));
+console.log(yearsUntilRetirement(1976, 'Vali'));
+console.log(yearsUntilRetirement(2011, 'Alex'));
+
+const calcAge = (birthYear, firstName) => {
+    age = 2024 - birthYear;
+    return `${firstName} is ${age} years old.`;
+}
+console.log(calcAge(2011, 'Alex'));
