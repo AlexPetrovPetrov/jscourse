1238a9b861f675bd540e962f5faf8b718e626e55
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-06-17 11:30:34 +0300

    using object methods

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -13,3 +13,23 @@ const calcAge = (birthYear, firstName) => {
     return `${firstName} is ${age} years old.`;
 }
 console.log(calcAge(2011, 'Alex'));
+
+//Using objects 
+const alex = {
+    firstName: 'Alex',
+    lastName: 'Petrov',
+    job: 'student',
+    age: 2024 - 2011,
+    birthYear: 2011,
+    hasDriversLicense: true,
+    calcAge: function () {
+        console.log(this);
+        this.age = 2024 - this.birthYear
+        return this.age;
+    },
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${alex.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    },
+}
+//Challenge
+console.log(alex.getSummary())
