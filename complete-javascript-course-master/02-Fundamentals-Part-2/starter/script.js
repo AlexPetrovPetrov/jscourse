13fce3f238d65c33d973f712212d3d8523641ea2
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-06-18 11:37:07 +0300

    doing exercise

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,35 +1,30 @@
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2024 - birthYear;
-    const retirement = 65 - age;
-    return `${firstName} retires in ${retirement} years.`;
-}
-console.log(yearsUntilRetirement(1971, 'Petar'));
-console.log(yearsUntilRetirement(1976, 'Vali'));
-console.log(yearsUntilRetirement(2011, 'Alex'));
 
-const calcAge = (birthYear, firstName) => {
-    age = 2024 - birthYear;
-    return `${firstName} is ${age} years old.`;
-}
-console.log(calcAge(2011, 'Alex'));
+};
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.92,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+mark.calcBMI();
+john.calcBMI();
 
-//Using objects 
-const alex = {
-    firstName: 'Alex',
-    lastName: 'Petrov',
-    job: 'student',
-    age: 2024 - 2011,
-    birthYear: 2011,
-    hasDriversLicense: true,
-    calcAge: function () {
-        console.log(this);
-        this.age = 2024 - this.birthYear
-        return this.age;
-    },
-    getSummary: function () {
-        return `${this.firstName} is a ${this.calcAge()}-year old ${alex.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
-    },
-}
-//Challenge
-console.log(alex.getSummary())
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI ${mark.bmi} is higher than ${john.fullName}'s BMI ${john.bmi}`)
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI ${john.bmi} is higher than ${mark.fullName}'sBMI ${mark.bmi}`)
+};
