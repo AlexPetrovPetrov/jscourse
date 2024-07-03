3b396f9fdda090610c85605fda82c78a321089b6
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-07-03 13:55:22 +0300

    doing exersice

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,15 +1,33 @@
-let performance = 1;
-while (performance <= 10) {
-    console.log(`The young rock star performed on stage ${performance} times in New York!!🤘`);
-    performance++;
-}
+const alex = [
+    'Alex',
+    'Petrov',
+    2024 - 2011,
+    'student',
+    ['Dinev', 'Yoan', 'Marti'],
+    true
+];
 
-let dice = Math.trunc(Math.random() * 6) + 1;
-while (dice !== 6) {
-    console.log(`The dice rolled and the number was ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6) {
-        console.log('The loop is about to end...🔚')
+for (let i = alex.length - 1; i >= 0; i--) {
+    console.log(i, alex[i]);
+}
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--------Starting exercise ${exercise}`)
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Lifting weight repetition ${rep}`)
     }
-
 }
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals)
