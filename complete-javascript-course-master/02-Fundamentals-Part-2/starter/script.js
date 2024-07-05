79ac7da6335a0e6cfe80974050e5a7674e9eaa2e
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2024-07-05 13:49:14 +0300

    making one exersice with ternary operator

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,33 +1,3 @@
-const alex = [
-    'Alex',
-    'Petrov',
-    2024 - 2011,
-    'student',
-    ['Dinev', 'Yoan', 'Marti'],
-    true
-];
-
-for (let i = alex.length - 1; i >= 0; i--) {
-    console.log(i, alex[i]);
-}
-for (let exercise = 1; exercise < 4; exercise++) {
-    console.log(`--------Starting exercise ${exercise}`)
-
-    for (let rep = 1; rep < 6; rep++) {
-        console.log(`Lifting weight repetition ${rep}`)
-    }
-}
-
-const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-
-for (i = 0; i < bills.length; i++) {
-    const tip = calcTip(bills[i]);
-    tips.push(tip);
-    totals.push(tip + bills[i]);
-}
-console.log(bills, tips, totals)
+const age 23;
+age => 18 ? console.log('You can drive a ccar.') :
+    console.log('You cannot drive a car')
