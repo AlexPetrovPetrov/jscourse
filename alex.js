5f8f39e208284c8b4c2c056575e4f68f298f51d3
Alex <alexpetrovpetrovsofia@gmail.com>
2024-12-23 11:20:50 +0200

    making my first function

--- alex.js
+++ alex.js
@@ -1,6 +1,21 @@
 let bankAccount = 1000000;
 if (bankAccount < 100000) {
      console.log("You are broke.");
-} else {
+} else if( bankAccount => 1000000) {
     console.log("You are rich.")
+} else {
+    console.log("You are broke.")
+};
+let myAge = 30;
+if (myAge < 18 || myAge > 30 || myAge === 23) {
+    console.log("You won't come to our holiday party.")
+} else {
+    console.log("You can come to our party.")
+};
+let martinsAge = 4;
+while (martinsAge < 20) {
+    martinsAge++;
+};
+for (bobosAge = 2; bobosAge < 18;bobosAge++ ) {
+    console.log("You are less than 18 years old.");
 };
