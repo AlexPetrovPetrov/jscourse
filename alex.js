580d5402ec95e6dea914bbf9bc4b82e259d7b5ce
Alex <alexpetrovpetrovsofia@gmail.com>
2024-12-25 14:21:25 +0200

    using date object

--- alex.js
+++ alex.js
@@ -37,10 +37,33 @@ if (!isNaN(a)) {
 }
 var myString = 'I\'m a "fun"string';
 console.log(myString);
-console.log(myString.toLocaleUpperCase);
-console.log(myString.toLocaleLowerCase);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
 console.log(myString.indexOf("string"));
-let meat = "pork, lamb, chicken, salami, ham"
+let meat = "pork, lamb, chicken, salami, ham";
 let meaArray = meat.split(",");
-let message = "Hello world"
-console.log(message.slice(2,8))
+let message = "Hello world";
+console.log(message.slice(2, 8));
+
+let dogBreeds = ["pomeranian", "husky", "poodle", 1];
+console.log(dogBreeds.length);
+console.log(dogBreeds.reverse());
+console.log(dogBreeds.sort());
+//An object is a collection of properties, and a property is an association between a name (or key) and a value. A property's value can be a function, in which case the property is known as a method. Objects in JavaScript, just as in many other programming languages, can be compared to objects in real life.
+let myCar = new Object();
+myCar.maxSpeed = 200;
+myCar.driver = "Alex";
+myCar.drive = function(speed,time){
+   let drivenDistance= speed * time;
+   console.log(drivenDistance);
+}
+myCar.drive(100,5);
+//The this keyword refers to the context where a piece of code, such as a function's body, is supposed to run. Most typically, it is used in object methods, where this refers to the object that the method is attached to, thus allowing the same method to be reused on different objects.
+
+let MyCar2 = function(topSpeed,chauffeur) {
+  this.topSpeed = topSpeed;
+  this.chauffeur = chauffeur;
+  this.guide = function(pace,tempo){
+   console.log(pace * tempo)
+  }
+}
