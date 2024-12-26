9f00489930ced39cca58ae6c771ef0f4bd1b558a
Alex <alexpetrovpetrovsofia@gmail.com>
2024-12-26 13:20:15 +0200

    making working place a bit cleaner

--- alex.js
+++ alex.js
@@ -51,7 +51,7 @@ console.log(dogBreeds.reverse());
 console.log(dogBreeds.sort());
 //An object is a collection of properties, and a property is an association between a name (or key) and a value. A property's value can be a function, in which case the property is known as a method. Objects in JavaScript, just as in many other programming languages, can be compared to objects in real life.
 let myCar = new Object();
-myCar.maxSpeed = 200;
+this.maxSpeed = 200;
 myCar.driver = "Alex";
 myCar.drive = function(speed,time){
    let drivenDistance= speed * time;
@@ -63,7 +63,10 @@ myCar.drive(100,5);
 let MyCar2 = function(topSpeed,chauffeur) {
   this.topSpeed = topSpeed;
   this.chauffeur = chauffeur;
-  this.guide = function(pace,tempo){
+  this.guide = function(pace,tempo) {
    console.log(pace * tempo)
-  }
-}
+  };
+};
+MyCar2(50,3);
+var myDate = new Date();
+console.log(myDate)
