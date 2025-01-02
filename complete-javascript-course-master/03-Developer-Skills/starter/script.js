80eac043c8e36b5e338700f2d99ad3c0ac51c06e
Alex Petrov <alexpetrovpetrovsofia@gmail.com>
2025-01-02 16:28:52 +0200

    making a innitial commit tonproceed making a cacl web app

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,23 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-const calcTempAmplitudeNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+const testData1 = [17, 21, 23];
+const testData2 = [12, 5, -5, 0, 4];
 
-  let max = temps(0);
-  let min = temps(0);
+console.log(
+  `...${testData1[0]}celsius ... ${testData1[1]} celsius ... ${testData1[2]} celsius ...`
+);
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}celsius in ${i + 1} days ... `;
   }
-  console.log(max, min);
-  return max - min;
+  console.log('...' + str);
 };
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
-console.log(amplitudeNew);
+printForecast(testData1);
